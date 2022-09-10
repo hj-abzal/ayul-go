52352e4f5a7ca1add8b7002250d6896bec790d88
nooreekra <rakhymzhan.ff@gmail.com>
2022-09-10 18:10:30 +0600

    changed img and size

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -11,7 +11,7 @@ export const Main = () => {
     useEffect(() => {
             gsap.timeline({ repeat:-1, repeatDelay:0, yoyo:true})
             .to('.m', {duration:(i)=>[0.8,1.3][i], y:-10266, ease:'steps(29)', stagger:-0.3}, 0)
-            .to('.frog', {duration:2, scale:1.1, transformOrigin:'50% 50%', ease:'power2', onComplete:swapMask}, 0)
+            .to('.frog', {duration:2, scale:1.05, transformOrigin:'50% 50%', ease:'power2', onComplete:swapMask}, 0)
         
             let currentMask = 1;
             function swapMask(){
@@ -33,16 +33,16 @@ export const Main = () => {
                 </p>
             </div>
             <div ref={el} className={s.animation}>
-                <svg viewBox="0 0 630 352" xmlns="http://www.w3.org/2000/svg">
+                <svg viewBox="0 0 450 352" xmlns="http://www.w3.org/1500/svg">
                     <mask id="m1">
                         <image class="m" href="https://assets.codepen.io/721952/liquidMask1.svg" y="-1" width="630" height="10620" />
                     </mask>
                     <mask id="m2">
                         <image class="m" className="maskImg" href="https://assets.codepen.io/721952/liquidMask1.svg" y="-1" width="630" height="10620" />
                     </mask>
-                    <image mask="url(#m2)" href="https://images.unsplash.com/photo-1597626564517-426e3c39ebbe?auto=format&fit=crop&w=1260&q=50" width="630" height="420" />
+                    <image mask="url(#m2)" href="https://auyl.kz/static_dir/img/poster.jpg" width="630" height="420" />
                     <g mask="url(#m1)">
-                        <image class="frog" href="https://images.unsplash.com/photo-1597626564517-426e3c39ebbe?auto=format&fit=crop&w=1260&q=50" width="630" height="420" />
+                        <image class="frog" href="https://auyl.kz/static_dir/img/poster.jpg" width="630" height="420" />
                     </g>
                 </svg>
             </div>
